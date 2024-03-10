02664efc1729625f21e92e6c6c0f93c969efd858
Ivan Belyaev <ibelyaevnmc@gmail.com>
2024-03-10 16:22:34 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3333;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3333);
+  await app.listen(PORT);
 }
 bootstrap();
